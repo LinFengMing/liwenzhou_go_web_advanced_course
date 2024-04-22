f277662b81fb815d8897f56d776daa1c64dc47be
jiro <jiro853141@gmail.com>
2024-04-23 01:03:22 +0800

    2.07

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
@@ -9,6 +10,84 @@ import (
 )
 
 var db *sqlx.DB
+
+type user struct {
+	ID   int    `db:"id"`
+	Age  int    `db:"age"`
+	Name string `db:"name"`
+}
+
+// 查詢單筆資料範例
+func queryRowDemo() {
+	sqlStr := "select id, name, age from user where id=?"
+	var u user
+	err := db.Get(&u, sqlStr, 1)
+	if err != nil {
+		fmt.Printf("get failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.ID, u.Name, u.Age)
+}
+
+// 查詢多筆資料範例
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from user where id > ?"
+	var users []user
+	err := db.Select(&users, sqlStr, 0)
+	if err != nil {
+		fmt.Printf("select failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("users:%#v\n", users)
+}
+
+// 新增資料範例
+func insertRowDemo() {
+	sqlStr := "insert into user(name, age) values(?, ?)"
+	ret, err := db.Exec(sqlStr, "alex", 38)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return
+	}
+	theID, err := ret.LastInsertId() // 新插入數據的ID
+	if err != nil {
+		fmt.Printf("get lastinsert ID failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is %d.\n", theID)
+}
+
+// 更新資料範例
+func updateRowDemo() {
+	sqlStr := "update user set age=? where id = ?"
+	ret, err := db.Exec(sqlStr, 39, 6)
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影響的行數
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("update success, affected rows:%d\n", n)
+}
+
+// 刪除資料範例
+func deleteRowDemo() {
+	sqlStr := "delete from user where id = ?"
+	ret, err := db.Exec(sqlStr, 6)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影響的行數
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
+}
 
 func initMySQL() (err error) {
 	dsn := "root:1qaz2wsx3edc@tcp(127.0.0.1:3306)/liwenzhou_go_web_advanced_course?charset=utf8mb4&parseTime=True"
@@ -22,10 +101,98 @@ func initMySQL() (err error) {
 	return
 }
 
+func insertUserDemo() (err error) {
+	_, err = db.NamedExec(`insert into user(name, age) values(:name, :age)`,
+		map[string]interface{}{
+			"name": "johnny",
+			"age":  28,
+		})
+	return
+}
+
+func namedQueryDemo() {
+	sqlStr := "select id, name, age from user where name=:name"
+	// 使用 map 參數
+	rows, err := db.NamedQuery(sqlStr, map[string]interface{}{"name": "johnny"})
+	if err != nil {
+		fmt.Printf("named query failed, err:%v\n", err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u user
+		err := rows.StructScan(&u)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			continue
+		}
+		fmt.Printf("user:%#v\n", u)
+	}
+	u := user{
+		Name: "johnny",
+	}
+	// 使用結構體參數
+	rows, err = db.NamedQuery(sqlStr, u)
+	if err != nil {
+		fmt.Printf("named query failed, err:%v\n", err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u user
+		err := rows.StructScan(&u)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			continue
+		}
+		fmt.Printf("user:%#v\n", u)
+	}
+}
+
+func transactionDemo() (err error) {
+	tx, err := db.Beginx()
+	if err != nil {
+		fmt.Printf("begin trans failed, err:%v\n", err)
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			fmt.Println("rollback")
+			tx.Rollback()
+		} else {
+			err = tx.Commit()
+			fmt.Println("commit")
+		}
+	}()
+	sqlStr1 := "update user set age=30 where id=?"
+	rs, err := tx.Exec(sqlStr1, 3)
+	if err != nil {
+		return err
+	}
+	n, err := rs.RowsAffected()
+	if n != 1 {
+		return errors.New("exec sql1 failed")
+	}
+	sqlStr2 := "update user set age=50 where id=?"
+	rs, err = tx.Exec(sqlStr2, 5)
+	if err != nil {
+		return err
+	}
+	n, err = rs.RowsAffected()
+	if n != 1 {
+		return errors.New("exec sql2 failed")
+	}
+	return nil
+}
+
 func main() {
 	if err := initMySQL(); err != nil {
 		fmt.Printf("init mysql failed, err:%v\n", err)
 		return
 	}
 	fmt.Println("connect to db success")
+	transactionDemo()
 }
