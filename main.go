f58fc48f33b3fd888c12ab67ed13f7702d8ed58b
jiro <jiro853141@gmail.com>
2024-04-24 17:36:30 +0800

    3.03

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"net"
 	"net/http"
+	"net/http/httputil"
+	"os"
+	"runtime/debug"
+	"strings"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -18,11 +25,6 @@ func InitLogger() {
 	logger = zap.New(core, zap.AddCaller())
 	sugarLogger = logger.Sugar()
 }
-
-// func getLogWriter() zapcore.WriteSyncer {
-// 	file, _ := os.OpenFile("./test.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0744)
-// 	return zapcore.AddSync(file)
-// }
 
 func getLogWriter() zapcore.WriteSyncer {
 	lumberJackLogger := &lumberjack.Logger{
@@ -52,25 +54,81 @@ func getEncoder() zapcore.Encoder {
 	return zapcore.NewJSONEncoder(encodingConfig)
 }
 
-func simpleHttpGet(url string) {
-	resp, err := http.Get(url)
-	if err != nil {
-		logger.Error("Error fetching URL",
-			zap.String("url", url),
-			zap.Error(err),
+// GinLogger 接收 gin 框架預設的日誌
+func GinLogger(logger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+
+		cost := time.Since(start)
+		logger.Info(path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			zap.Duration("cost", cost),
 		)
-	} else {
-		logger.Info("Successfully fetched URL",
-			zap.String("url", url),
-			zap.Int("status", resp.StatusCode),
-		)
-		resp.Body.Close()
+	}
+}
+
+// GinRecovery recover 掉项目可能出现的 panic
+func GinRecovery(logger *zap.Logger, stack bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				// Check for a broken connection, as it is not really a
+				// condition that warrants a panic stack trace.
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					if se, ok := ne.Err.(*os.SyscallError); ok {
+						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") || strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+
+				httpRequest, _ := httputil.DumpRequest(c.Request, false)
+				if brokenPipe {
+					logger.Error(c.Request.URL.Path,
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+					// If the connection is dead, we can't write a status to it.
+					c.Error(err.(error)) // nolint: errcheck
+					c.Abort()
+					return
+				}
+
+				if stack {
+					logger.Error("[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+						zap.String("stack", string(debug.Stack())),
+					)
+				} else {
+					logger.Error("[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+				}
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+		}()
+		c.Next()
 	}
 }
 
 func main() {
 	InitLogger()
-	defer logger.Sync()
-	simpleHttpGet("www.google.com")
-	simpleHttpGet("http://www.google.com")
+	r := gin.New()
+	r.Use(GinLogger(logger), GinRecovery(logger, true))
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	})
+	r.Run()
 }
