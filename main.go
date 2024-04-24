998757825d7dc63e66920359885ef42e8b5e5eaa
jiro <jiro853141@gmail.com>
2024-04-24 16:38:10 +0800

    3.02

--- main.go
+++ main.go
@@ -3,13 +3,53 @@ package main
 import (
 	"net/http"
 
+	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var logger *zap.Logger
+var sugarLogger *zap.SugaredLogger
 
 func InitLogger() {
-	logger, _ = zap.NewProduction()
+	writeSyncer := getLogWriter()
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+	logger = zap.New(core, zap.AddCaller())
+	sugarLogger = logger.Sugar()
+}
+
+// func getLogWriter() zapcore.WriteSyncer {
+// 	file, _ := os.OpenFile("./test.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0744)
+// 	return zapcore.AddSync(file)
+// }
+
+func getLogWriter() zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   "./test.log",
+		MaxSize:    200, // megabytes
+		MaxBackups: 5,
+		MaxAge:     30, // days
+		Compress:   false,
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
+
+func getEncoder() zapcore.Encoder {
+	encodingConfig := zapcore.EncoderConfig{
+		TimeKey:        "ts",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+	return zapcore.NewJSONEncoder(encodingConfig)
 }
 
 func simpleHttpGet(url string) {
