8b6e091864c3abc8196cb982ed68948c8ab4fc48
jiro <jiro853141@gmail.com>
2024-04-26 23:50:09 +0800

    3.05

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Config struct {
+	Port        int    `mapstructure:"port"`
+	Version     string `mapstructure:"version"`
+	MySQLConfig `mapstructure:"mysql"`
+}
+
+type MySQLConfig struct {
+	Host   string `mapstructure:"host"`
+	DbName string `mapstructure:"dbname"`
+	Port   int    `mapstructure:"port"`
+}
+
 func main() {
 	viper.SetDefault("fileDir", "./")
 	viper.SetConfigName("config")
@@ -23,6 +35,12 @@ func main() {
 	viper.OnConfigChange(func(e fsnotify.Event) {
 		fmt.Println("Config file changed:", e.Name)
 	})
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		fmt.Printf("viper.Unmarshal failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("config:%#v\n", config)
 	r := gin.Default()
 	r.GET("/version", func(c *gin.Context) {
 		c.String(200, viper.GetString("version"))
