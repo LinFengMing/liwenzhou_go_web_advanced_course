292be9ee39ea419ac9d8f900c3628e7a4eb1a2ab
jiro <jiro853141@gmail.com>
2024-05-22 01:08:38 +0800

    4.27

--- logic/post.go
+++ logic/post.go
@@ -4,6 +4,8 @@ import (
 	"gin_demo/dao/mysql"
 	"gin_demo/models"
 	"gin_demo/pkg/snowflake"
+
+	"go.uber.org/zap"
 )
 
 func CreatePost(p *models.Post) (err error) {
@@ -13,6 +15,30 @@ func CreatePost(p *models.Post) (err error) {
 	return mysql.CreatePost(p)
 }
 
-func GetPostById(pid int64) (data *models.Post, err error) {
-	return mysql.GetPostById(pid)
+func GetPostById(pid int64) (data *models.ApiPostDetail, err error) {
+	// 查詢並組合資料
+	post, err := mysql.GetPostById(pid)
+	if err != nil {
+		zap.L().Error("mysql.GetPostById(pid) failed", zap.Int64("pid", pid), zap.Error(err))
+		return
+	}
+	// 查詢作者
+	user, err := mysql.GetUserById(post.AuthorID)
+	if err != nil {
+		zap.L().Error("mysql.GetUserById(post.AuthorID) failed", zap.Int64("author_id", post.AuthorID), zap.Error(err))
+		return
+	}
+	// 查詢社區詳細資料
+	communityDetail, err := mysql.GetCommunityDetailByID(post.CommunityID)
+	if err != nil {
+		zap.L().Error("mysql.GetCommunityDetailByID(post.CommunityID) failed", zap.Int64("community_id", post.CommunityID), zap.Error(err))
+		return
+	}
+	// 回傳組合資料
+	data = &models.ApiPostDetail{
+		AuthorName:      user.Username,
+		Post:            post,
+		CommunityDetail: communityDetail,
+	}
+	return
 }
--- models/post.go
+++ models/post.go
@@ -4,10 +4,16 @@ import "time"
 
 type Post struct {
 	ID          int64     `json:"id" db:"post_id"`
-	AuthorID    int64     `json:"author" db:"author_id"`
+	AuthorID    int64     `json:"author_id" db:"author_id"`
 	CommunityID int64     `json:"community_id" db:"community_id" binding:"required"`
 	Status      int32     `json:"status" db:"status"`
 	Title       string    `json:"title" db:"title" binding:"required"`
 	Content     string    `json:"content" db:"content" binding:"required"`
 	CreateTime  time.Time `json:"create_time" db:"create_time"`
+}
+
+type ApiPostDetail struct {
+	AuthorName       string `json:"author_name"`
+	*Post            `json:"post"`
+	*CommunityDetail `json:"community"`
 }
